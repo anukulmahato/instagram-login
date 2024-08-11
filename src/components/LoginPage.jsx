f675e860da97565e5e7605c15379063633d105c5
Anukul <anukulmahato014@gmail.com>
2024-08-11 12:42:58 +0600

    inital all file upload in github

---
+++ src/components/LoginPage.jsx
@@ -0,0 +1,62 @@
+const LoginPage = () => {
+    return(
+        <>
+            <div className="instagramSection">
+                <div className="instagram_wrapper">
+                    <div className="left_column">
+                        <div className="insta_image">
+                            <img src="https://media.canva.com/v2/image-resize/format:PNG/height:541/quality:100/uri:s3%3A%2F%2Fmedia-private.canva.com%2FuV-l4%2FMAGNeKuV-l4%2F1%2Fp.png/watermark:F/width:250?csig=AAAAAAAAAAAAAAAAAAAAAKi0wWcznz0uTdhtNbzLbydxNgwT4seHapxDEpNiBkme&exp=1723321803&osig=AAAAAAAAAAAAAAAAAAAAAEQ0qG1qVF9s8VORZeUj6-MaFOGvNYSk-IIlVx3_KMFZ&signer=media-rpc&x-canva-quality=thumbnail_large" alt="" />
+                        </div>
+                    </div>
+                    <div className="right_column">
+                        <div className="itemBox">
+                            <div className="logo">
+                               <img src="https://media.canva.com/v2/image-resize/format:PNG/height:51/quality:100/uri:s3%3A%2F%2Fmedia-private.canva.com%2FQHXnc%2FMAGNhHQHXnc%2F1%2Fp.png/watermark:F/width:174?csig=AAAAAAAAAAAAAAAAAAAAAF97PuFPDLg4qjNldljSdjvaerFK6dN0gxMsXEKgDLdE&exp=1723366486&osig=AAAAAAAAAAAAAAAAAAAAADfp57Z_2dXTJWvqx5Dh3bwyV_fVT29rKvudCEwO5Mmb&signer=media-rpc&x-canva-quality=thumbnail_large" alt="" /> 
+                            </div>
+                            
+                            <img className="user" src="https://lh3.googleusercontent.com/p/AF1QipOfBgQae8edsAzIwXZXx_4lKqfO1ssj4Clj5zvU=s680-w680-h510" alt="" />
+                            <button>Continue as_anukul_mahato_</button>
+                            <span>Not_anukul_mahato_? <a href="#">Switch Accounts</a></span>
+                        </div>
+                    </div>
+                </div>
+                <div className="footer">
+                    <div className="footer_menu">
+                        <a href="#">Meta</a>
+                        <a href="#">About</a>
+                        <a href="#">Blog</a>
+                        <a href="#">Jobs</a>
+                        <a href="#">Help</a>
+                        <a href="#">API</a>
+                        <a href="#">Privacy</a>
+                        <a href="#">Terms</a>
+                        <a href="#">Locations</a>
+                        <a href="#">Instagram Lite</a>
+                        <a href="#">Threads</a>
+                        <a href="#">Contact uploading and non-users</a>
+                        <a href="#">Meta verified</a>
+                    </div>
+                    <div className="footer_bottom">
+                        <span>
+                            <select>
+                            <option >Bangladesh</option>
+                            <option >Čeština</option>
+                            <option >Dansk</option>
+                            <option >Deutsch</option>
+                            <option >Ελληνικά</option>
+                            <option >English</option>
+                            <option >Русский</option>
+                            <option >Svenska</option>
+                            <option >ภาษาไทย</option>
+                            <option >Filipino</option>
+                            </select>
+                        </span>
+                        <p>© 2024 Instagram from Meta</p>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default LoginPage;
---
+++ src/main.jsx
@@ -0,0 +1,5 @@
+import ReactDOM from "react-dom/client";
+import LoginPage from "./components/LoginPage";
+import "./instaLogin.css";
+
+ReactDOM.createRoot(document.getElementById("instagram")).render(<LoginPage/>);
